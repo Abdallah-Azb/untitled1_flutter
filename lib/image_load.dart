dd43d33c9c65af53edddaee8aaf38dc45bb03f70
abdallahix <a.azab@ix-sol.com>
2023-01-02 12:32:30 +0200

    test

--- lib/image_load.dart
+++ lib/image_load.dart
@@ -33,3 +33,109 @@
 //     return provider.loadBuffer(key, decode);
 //   }
 // }
+
+
+import 'dart:typed_data';
+import 'package:bit_array/bit_array.dart';
+import 'dart:math' as Math;
+
+import 'package:byte_util/byte_array.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image/image.dart';
+import 'package:mqtt5_client/mqtt5_client.dart';
+// import 'package:bit_array/bit_array.dart';
+
+class JpegQueue {
+
+
+  static int vSeq = 0;
+  static   Set<BitSet> vPresent = <BitSet>{};
+  static ByteArray ?  vData = ByteArray(Uint8List(64 * 1024)) ;
+  static enqueue (int seq ,ByteArray data,int length ){
+    ByteBuffer byteBuffer  ;
+    byteBuffer = data.bytes.buffer ;
+    MqttByteBuffer mqttByteBuffer =MqttByteBuffer.fromList(data.bytes);
+    if (seq > vSeq) {
+
+      print(" seq > vSeq ");
+
+       vPresent.clear();
+      vSeq = seq;
+      vData = null ;
+    }
+
+    print("=== mqttByteBuffer.position 0000   ====     ${mqttByteBuffer.position}");
+    if(seq == vSeq){
+      mqttByteBuffer.read(6);
+      // data.array.
+
+      // print("=== mqttByteBuffer.position 11111   ====     ${mqttByteBuffer.position}");
+
+    }
+
+
+
+  }
+  // BitArray v = BitArray(1) ;
+
+
+
+
+
+
+
+
+ // ByteBuffer vData = ByteBuffer(64 * 1024);
+/*
+  static Uint8List enqueue(int seq , Uint8List dataAfterDecrypt)   {
+      int vSeq = 0;
+      Uint8List vData = Uint8List(64 * 1024);
+
+    // static Set<BitSet>  vPresent  ={};
+       BitArray vPresent = new BitArray(1024);
+
+    // if (seq > vSeq) {
+      vPresent.clearAll();
+      vSeq = seq;
+      vData = Uint8List(0);
+    // }
+    // if (seq == vSeq) {
+      // Position to 6 like java
+      int offset = dataAfterDecrypt.offsetInBytes;
+      int imageLen = dataAfterDecrypt.buffer.asByteData().getInt32(offset + 6);
+      int remaining = dataAfterDecrypt.buffer.asByteData().lengthInBytes ~/ 2;
+      // if (vData.isEmpty || imageLen != vData.length) {
+        vPresent.clearAll();
+        print("imageLen imageLen imageLen    $imageLen");
+        vData = Uint8List(imageLen);
+        print("vData vData vData vData vData vData vData   $vData");
+      // }
+      int imageOffset = dataAfterDecrypt.buffer.asByteData().getInt32(offset + 6);
+
+
+      // while (remaining > 0) {
+      //   int blockSize = Math.min(remaining, 256);
+      //
+      //   Uint8List.view(dataAfterDecrypt.buffer,blockSize);
+      //   vPresent.setBit(imageOffset ~/ 256);
+      //   // vPresent.add(BitSet(imageOffset ~/ 256));
+      //   imageOffset += blockSize;
+      //  // print(">>>>>>>>>>>>>>>   $imageOffset");
+      //   // bb.get(vData, imageOffset, blockSize);
+      //   // imageOffset += blockSize;
+      // }
+      vPresent.clearBit(0);
+
+
+
+
+    // }
+    return   vData ;
+  }
+*/
+
+
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,25 @@ import 'dart:developer';
 import 'dart:math' as Math;
 import 'dart:typed_data';
 import 'package:at_commons/at_commons.dart';
+import 'package:bit_array/bit_array.dart';
+import 'package:byte_util/byte.dart';
+import 'package:byte_util/byte_array.dart';
 import 'package:dio/dio.dart';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_sodium/flutter_sodium.dart';
+// import 'package:image/image.dart';
+
+import 'image_load.dart';
 
 void main() {
+  Sodium.init();
+
   runApp(const MyApp());
 }
-
+//
+String token = "Bearer 4495e7fb983d031e194120efb33840487eb8faf71c31eac69ad752ee3fc38986";
+//
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -66,12 +76,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Uint8List? dataAfterDecrypt;
 
   void _incrementCounter() async {
-    //  datagramSocket = DatagramSocket.;
     requestLiveInfo();
   }
 
   void sendSubscribe(bool unsubscribe) {
-    print("sendSubscribe");
+    // print("sendSubscribe");
     ByteData subscribe = ByteData(0);
     String subscribeString = subscribe.toString();
     var videoType = UdpConstants.PACKET_NO_VIDEO;
@@ -87,27 +96,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
     bb.addByte(session.length);
     bb.append(session);
-    bb.addByte(4); // flag vedio
+    bb.addByte(5); //  flag state + flag vedio
     bb.addByte(52); // videoType
-    bb.addByte(47); // audioType
+    bb.addByte(47); // audioType enabled
 
     subscribeSeq++;
     sendPacket(bb.getData());
-    requestedFlags = subscribe.buffer.lengthInBytes;
+
+    requestedFlags = 5;
+
   }
 
   ///
   sendPacket(List<int> buffer) async {
-    print("sendPacket");
-    print(buffer);
+    // print("sendPacket");
+    // print(buffer);
     var d = await datagramSocket!.send(buffer, InternetAddress(host), port);
-    print("sendPacket sendPacket $d");
+    // print("sendPacket sendPacket $d");
   }
 
   requestLiveInfo() async {
     responseDataRequest = "Waiting ......";
     setState(() {});
-    String token = "Bearer c4ec86150aeb1840a2ddb79be611c8690594b9d48ba0a0b473e1e2bf1432a9c3";
 
     String liveInfoApi = "https://api.doorbird.io/live/info";
 
@@ -124,7 +134,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Map<String, dynamic> jsonData = json.decode(response.toString());
         sessionId = jsonData["video"]["cloud"]["mjpg"]["default"]["session"];
         keyEncepted = jsonData["video"]["cloud"]["mjpg"]["default"]["key"];
-        print("keyEncepted    ==>   $keyEncepted");
+        // print("keyEncepted    ==>   $keyEncepted");
 
         runCamera();
       }
@@ -147,10 +157,17 @@ class _MyHomePageState extends State<MyHomePage> {
     //
     var lastSubscribe = 0;
     //
+
     Timer.periodic(
       const Duration(seconds: 2),
       (timer) {
-        sendSubscribe(false);
+
+        if (lastSubscribe + (currentFlags == requestedFlags ? 15000 : 500) < DateTime.now().millisecondsSinceEpoch) {
+          print("====  DateTime.now().millisecondsSinceEpoch =====     "+  DateTime.now().millisecondsSinceEpoch.toString());
+          lastSubscribe =  DateTime.now().millisecondsSinceEpoch;
+          sendSubscribe(false);
+        }
+      //  sendSubscribe(false);
 
         datagramSocket.timeout(const Duration(milliseconds: 1000));
 
@@ -162,7 +179,96 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  // Uint8List? imageeeee = Uint8List(64 * 1024);
+
+
+
   processPacket(Datagram datagram) {
+    // Uint8List packetData =  datagram.data ;
+    ByteArray packetData = ByteArray(datagram.data)  ;
+    int dataLength = datagram.data.length ;
+
+    Byte type = packetData.array.first ;
+
+    ByteArray ? data ;
+    // print("== type00 ===     $type"  ) ;
+    // print("== type value00 ===     ${type.value}"  ) ;
+    if(type.value == UdpConstants.PACKET_ENCRYPTION_TYPE_1.value){ // 225  &-OR-& -31
+      // decrypt the data before handling them
+      ByteArray nonce =  ByteArray(packetData.bytes.sublist(1,9));
+      ByteArray encryptedData =  ByteArray(packetData.bytes.sublist(nonce.array.length+1));
+      ByteArray key =  ByteArray(toUnit8List(keyEncepted!.codeUnits));
+
+      print("== nonce ===     $nonce"  ) ;
+      print("== nonce array ===     ${nonce.array}"  ) ;
+      print("== nonce array length ===     ${nonce.array.length}"  ) ;
+      print("== nonce bytes===     ${nonce.bytes}"  ) ;
+      print("== nonce bytes length===     ${nonce.bytes.length}"  ) ;
+      print("== nonce bytes lengthInBytes ===     ${nonce.bytes.lengthInBytes}"  ) ;
+      print("== nonce bytes offsetInBytes ===     ${nonce.bytes.offsetInBytes}"  ) ;
+      print("== nonce bytes buffer ===     ${nonce.bytes.buffer}"  ) ;
+      print("========================================================");
+      print("========================================================");
+      print("== encryptedData ===     $encryptedData"  ) ;
+      print("== encryptedData array ===     ${encryptedData.array}"  ) ;
+      print("== encryptedData array length ===     ${encryptedData.array.length}"  ) ;
+      print("== encryptedData bytes===     ${encryptedData.bytes}"  ) ;
+      print("== encryptedData bytes length===     ${encryptedData.bytes.length}"  ) ;
+      print("== encryptedData bytes lengthInBytes ===     ${encryptedData.bytes.lengthInBytes}"  ) ;
+      print("== encryptedData bytes offsetInBytes ===     ${encryptedData.bytes.offsetInBytes}"  ) ;
+      print("== encryptedData bytes buffer ===     ${encryptedData.bytes.buffer}"  ) ;
+      print("== key ===     $key"  ) ;
+
+
+      try{
+        data = ByteArray(toUnit8List(HelperIncreptionUsingSodiom.decrypt(cipherText: encryptedData.bytes, nonce: nonce.bytes, key: key.bytes)));
+      }catch(e){
+        print("== Catch Error in Decrypt Data In Sodiom") ;
+      }
+
+      if(data != null ){
+        dataLength = data.array.length;
+        type = data.array.first ;
+        int seq = ((data.array[1].value & 0xff) << 16) | ((data.array[2].value & 0xff) << 8) | ((data.array[3].value & 0xff));
+        Byte state =  data.array[4] ;
+        int flags = data.array[5].value;
+
+
+        print("========================================================");
+        print("========================================================");
+        // print("== data ===     $data"  ) ;
+        print("== data array===     ${data.array}"  ) ;
+        print("== data array length ===     ${data.array.length}"  ) ;
+        print("== data bytes===     ${data.bytes}"  ) ;
+        print("== data bytes buffer===     ${data.bytes.buffer}"  ) ;
+        print("== data bytes length===     ${data.bytes.length}"  ) ;
+        print("========================================================");
+        print("========================================================");
+        print("== type ===     $type"  ) ;
+        print("== type value ===     ${type.value}"  ) ;
+        print("========================================================");
+        print("========================================================");
+        print("== seq ===     $seq"  ) ;
+        print("========================================================");
+        print("========================================================");
+        print("== state ===     $state"  ) ;
+        print("== state value ===     ${state.value}"  ) ;
+        print("========================================================");
+        print("========================================================");
+        print("== flags ===     $flags"  ) ;
+
+        /// Image
+        if(type.value == UdpConstants.PACKET_JPEG_V2.value){ // 52
+
+          JpegQueue.enqueue(seq, data,dataLength);
+        }
+
+      }
+
+    }
+
+
+   /* ///
     print("processPacket ==>   ${datagram.data}");
     // encropt(datagram.data.toString(), keyEncepted!);
 
@@ -183,60 +289,70 @@ class _MyHomePageState extends State<MyHomePage> {
       log("dataAfterIncrypt ==>>>     $dataAfterDecrypt");
       dataLength = dataAfterDecrypt!.length;
       log("dataLengthNew ==>>    $dataLength");
-      int seq = ((dataAfterDecrypt![1] & 0xff) << 16) | ((dataAfterDecrypt![2] & 0xff) << 8) | ((dataAfterDecrypt![3] & 0xff));
+      int seq = ((dataAfterDecrypt![1] & 0xff) << 16) |
+          ((dataAfterDecrypt![2] & 0xff) << 8) |
+          ((dataAfterDecrypt![3] & 0xff));
       print("seq ===>>>    $seq");
-
-      enqueue(seq);
-
-      // if (seq != 0) {
-      //   setState(() {});
-      // }
-      // enqueue(seq, dataAfterIncrypt!) ;
-      // imageProvider =MemoryImage(dataAfterIncrypt!);
-      // imageProvider.
-    }
-  }
-
-  // private BitSet vPresent = new BitSet();
-  // private byte[] vData = new byte[64 * 1024];
-  int vSeq = 0;
-  Uint8List vData = Uint8List(64 * 1024);
-
-  Set vPresent = {};
-
-  void enqueue(int seq) async {
-    if (seq > vSeq) {
-      vPresent.clear();
-      vSeq = seq;
-      vData = Uint8List(0);
-    }
-    if (seq == vSeq) {
-      // Position to 6 like java
       int offset = dataAfterDecrypt!.offsetInBytes;
       int imageLen = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
       int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
-      if (vData.isEmpty || imageLen != vData.length) {
-        vPresent.clear();
-        vData = Uint8List(imageLen);
+
+print("imageLen ==>>   $imageLen");
+print("remaining ==>>   $remaining");
+print("offset ==>>   $offset");
+
+      *//*if(seq > vSeq){
+        imageeeee = null ;
+        vPresent.clearAll() ;
+        vSeq = seq;
       }
-      int imageOffset = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
-      int blockSize = Math.min(remaining, 256);
+      if(seq == vSeq){
+        int offset = dataAfterDecrypt!.offsetInBytes;
+        int imageLen = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
+        int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
 
-      // while (remaining > 0) {
-      //   Uint8List.view(dataAfterIncrypt!.buffer,blockSize);
-      //   vPresent.add(imageOffset / 256);
-      //   imageOffset += blockSize;
-      //   print(">>>>>>>>>>>>>>>   $imageOffset");
-      //   // bb.get(vData, imageOffset, blockSize);
-      //   // imageOffset += blockSize;
-      // }
 
-    }
+
+      }*//*
+
+      setState(() {});
+    }*/
   }
 
   ///
-  // ImageProvider ? imageProvider ;
-  // AssetBundle ?assetBundle ;
+
+  int vSeq = 0;
+
+  Set<Byte> vPresent =<Byte>{};
+
+
+/*
+  imageEnqueue (Uint8List data , int length,int seq){
+    // Uint8List vData =   Uint8List(64 * 1024);
+    BitArray ?vData =   BitArray(64 * 1024);
+    InputBuffer inputBuffer = InputBuffer(data.buffer.asUint8List(),offset: 0,length:length );
+    InputBuffer inputBufferNew ;
+    // int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
+
+    // = InputBuffer(data.buffer.asUint8List(),offset: 0,length:length );
+    if(seq > vSeq){
+      vPresent.clear();
+      vSeq = seq;
+      vData.clearAll();
+      vData = null ;
+    }
+    if(seq == vSeq){
+
+    }
+
+  }
+*/
+
+  ///
+
+
+
+  ///
   Uint8List toUnit8List(List<int> data) {
     return Uint8List.fromList(data);
   }
@@ -262,7 +378,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            dataAfterDecrypt == null ? SizedBox() : Expanded(child: Image.memory(Uint8List.fromList(dataAfterDecrypt!.buffer.asUint8List())))
+            // imageeeee == null ? SizedBox() : Expanded(child: Image.memory(Uint8List.fromList(imageeeee!)))
+          Expanded(child: Image.memory(Uint8List.fromList(fakwDataImage))),
           ],
         ),
       ),
@@ -280,27 +397,136 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+//
 
+
+///
+
+
+//
 class UdpConstants {
   static ByteData PACKET_SUBSCRIBE = ByteData(0x01);
-  static ByteData PACKET_STATE_CHANGE = ByteData(0x11);
-  static ByteData PACKET_ULAW = ByteData(0x21);
-  static ByteData PACKET_NO_AUDIO = ByteData(0x2F);
-  static ByteData PACKET_JPEG_V2 = ByteData(0x34);
-  static ByteData PACKET_NO_VIDEO = ByteData(0x3F);
-  static ByteData PACKET_ENCRYPTION_TYPE_1 = ByteData(0xE1); //225
+  static   Byte PACKET_STATE_CHANGE =  Byte(0x11); //17
+  static Byte PACKET_ULAW = Byte(0x21); // 33
+  static Byte PACKET_NO_AUDIO = Byte(0x2F);
+  static Byte PACKET_JPEG_V2 = Byte(0x34); // 52
+  static Byte PACKET_NO_VIDEO = Byte(0x3F);
+  static Byte PACKET_ENCRYPTION_TYPE_1 = Byte(0xE1); //225
   static ByteData FLAG_STATE = ByteData(1);
-  static ByteData FLAG_AUDIO = ByteData(2);
-  static ByteData FLAG_VIDEO = ByteData(4);
-  static ByteData STATE_VIDEO_SESSION_INVALID = ByteData(5);
-  static ByteData STATE_AUDIO_SESSION_INVALID = ByteData(6);
+  static Byte FLAG_AUDIO = Byte(2);
+  static Byte FLAG_VIDEO = Byte(4);
+  static Byte STATE_VIDEO_SESSION_INVALID = Byte(5);
+  static Byte STATE_AUDIO_SESSION_INVALID = Byte(6);
 }
+//
 
+///
+
+//
 class HelperIncreptionUsingSodiom {
-  static Uint8List decrypt({required Uint8List cipherText, required Uint8List nonce, required Uint8List key}) =>
+  static Uint8List decrypt(
+          {required Uint8List cipherText, required Uint8List nonce, required Uint8List key}) =>
       Sodium.cryptoAeadChacha20poly1305Decrypt(null, cipherText, null, nonce, key);
 
-// static Uint8List decrypt(
-//         {required Uint8List cipherText, required Uint8List nonce, required Uint8List key}) =>
-//     ChaCha20Poly1305.decrypt(cipherText, nonce, key);
 }
+
+
+
+/*
+processPacket(Datagram datagram) {
+  print("processPacket ==>   ${datagram.data}");
+  // encropt(datagram.data.toString(), keyEncepted!);
+
+  // try{
+  int dataLength = datagram.data.length;
+  Uint8List nonce = datagram.data.sublist(1, 9);
+  Uint8List cipherText = datagram.data.sublist(nonce.length + 1);
+  Uint8List key = toUnit8List(keyEncepted!.codeUnits);
+
+  print(" datagram.data ==>>   ${datagram.data}");
+  print(" datagram.data.length ==>>   $dataLength");
+  print(" cipherText ==>>   $cipherText");
+  print(" nonce ==>>   $nonce");
+  print(" key ==>>   $key");
+  dataAfterDecrypt = HelperIncreptionUsingSodiom.decrypt(cipherText: cipherText, nonce: nonce, key: key);
+
+  if (dataAfterDecrypt != null) {
+    log("dataAfterIncrypt ==>>>     $dataAfterDecrypt");
+    dataLength = dataAfterDecrypt!.length;
+    log("dataLengthNew ==>>    $dataLength");
+    int seq = ((dataAfterDecrypt![1] & 0xff) << 16) |
+    ((dataAfterDecrypt![2] & 0xff) << 8) |
+    ((dataAfterDecrypt![3] & 0xff));
+    print("seq ===>>>    $seq");
+    int offset = dataAfterDecrypt!.offsetInBytes;
+    int imageLen = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
+    int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
+
+    print("imageLen ==>>   $imageLen");
+    print("remaining ==>>   $remaining");
+    print("offset ==>>   $offset");
+
+    */
+/*if(seq > vSeq){
+        imageeeee = null ;
+        vPresent.clearAll() ;
+        vSeq = seq;
+      }
+      if(seq == vSeq){
+        int offset = dataAfterDecrypt!.offsetInBytes;
+        int imageLen = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
+        int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
+
+
+
+      }*//*
+
+
+    setState(() {});
+  }
+}
+
+///
+
+// void enqueue(int seq) async {
+  //   if (seq > vSeq) {
+  //     vPresent.clear();
+  //     vSeq = seq;
+  //     vData = Uint8List(0);
+  //   }
+  //   if (seq == vSeq) {
+  //     // Position to 6 like java
+  //     int offset = dataAfterDecrypt!.offsetInBytes;
+  //     int imageLen = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
+  //     int remaining = dataAfterDecrypt!.buffer.asByteData().lengthInBytes ~/ 2;
+  //     if (vData.isEmpty || imageLen != vData.length) {
+  //       vPresent.clear();
+  //       vData = Uint8List(imageLen);
+  //     }
+  //     int imageOffset = dataAfterDecrypt!.buffer.asByteData().getInt32(offset + 6);
+  //
+  //
+  //     while (remaining > 0) {
+  //       int blockSize = Math.min(remaining, 256);
+  //
+  //       Uint8List.view(dataAfterDecrypt!.buffer,blockSize);
+  //       vPresent.add(ByteData(imageOffset ~/ 256));
+  //       imageOffset += blockSize;
+  //       print(">>>>>>>>>>>>>>>   $imageOffset");
+  //       // bb.get(vData, imageOffset, blockSize);
+  //       // imageOffset += blockSize;
+  //     }
+  //
+  //
+  //
+  //
+  //   }
+  // }
+
+*/
+
+
+
+List<int> fakwDataImage =
+
+[-1, -40, -1, -32, 0, 16, 74, 70, 73, 70, 0, 1, 2, 1, 0, 96, 0, 96, 0, 0, -1, -37, 0, -124, 0, 13, 9, 10, 11, 10, 8, 13, 11, 10, 11, 14, 14, 13, 15, 19, 32, 21, 19, 18, 18, 19, 39, 28, 30, 23, 32, 46, 41, 49, 48, 46, 41, 45, 44, 51, 58, 74, 62, 51, 54, 70, 55, 44, 45, 64, 87, 65, 70, 76, 78, 82, 83, 82, 50, 62, 90, 97, 90, 80, 96, 74, 81, 82, 79, 1, 14, 14, 14, 19, 17, 19, 38, 21, 21, 38, 79, 53, 45, 53, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, -1, -60, 1, -94, 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 16, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 125, 1, 2, 3, 0, 4, 17, 5, 18, 33, 49, 65, 6, 19, 81, 97, 7, 34, 113, 20, 50, -127, -111, -95, 8, 35, 66, -79, -63, 21, 82, -47, -16, 36, 51, 98, 114, -126, 9, 10, 22, 23, 24, 25, 26, 37, 38, 39, 40, 41, 42, 52, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, -125, -124, -123, -122, -121, -120, -119, -118, -110, -109, -108, -107, -106, -105, -104, -103, -102, -94, -93, -92, -91, -90, -89, -88, -87, -86, -78, -77, -76, -75, -74, -73, -72, -71, -70, -62, -61, -60, -59, -58, -57, -56, -55, -54, -46, -45, -44, -43, -42, -41, -40, -39, -38, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, 17, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 119, 0, 1, 2, 3, 17, 4, 5, 33, 49, 6, 18, 65, 81, 7, 97, 113, 19, 34, 50, -127, 8, 20, 66, -111, -95, -79, -63, 9, 35, 51, 82, -16, 21, 98, 114, -47, 10, 22, 36, 52, -31, 37, -15, 23, 24, 25, 26, 38, 39, 40, 41, 42, 53, 54, 55, 56, 57, 58, 67, 68, 69, 70, 71, 72, 73, 74, 83, 84, 85, 86, 87, 88, 89, 90, 99, 100, 101, 102, 103, 104, 105, 106, 115, 116, 117, 118, 119, 120, 121, 122, -126, -125, -124, -123, -122, -121, -120, -119, -118, -110];
